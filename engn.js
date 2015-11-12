bb8664011f64154f1b30ef56aa061849f36e763e
htmlguy <htmlguy@users.noreply.github.com>
2015-11-11 21:01:15 -0500

    Update engn.js

--- engn.js
+++ engn.js
@@ -1,1 +1,17 @@
-
+// engn.js - A simple game utilities library.
+// Licensed under the MIT license.
+// Copyright (C) 2015 Podium
+// GitHub Repository: podiumnet/engn.js
+(function(window, document, undefined){
+  // Create the EngnJS object if it doesn't exist.
+  window.engn = window.engn || {};
+  
+  // Checks for intersection. Adapted from http://stackoverflow.com/questions/2752349/fast-rectangle-to-rectangle-intersection.
+  window.engn.checkIntersection = function (xa, ya, wa, ha, xb, yb, wb, hb) {
+    var xxa = xa + wa;
+    var yya = ya + ha;
+    var xxb = xb + wb;
+    var yyb = yb + hb;
+    return !(xb > xxa || xxb < xa || yb > yya || yyb < ya);
+  };
+})(this, this.document);
